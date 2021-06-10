09e032044418c5cb60a4dde4dce350ffb16fae2c
Francisco Viana <franciscoviana862@gmail.com>
2021-06-10 07:24:17 -0300

    add interface Storage

---
+++ src/app/contracts/storage.js
@@ -0,0 +1,11 @@
+module.exports = class Storage {
+  /*
+  * @param {String} fileName
+  * @param {String} path
+  * @param {String} content
+  * @returns {Boolean}
+  */
+  store(fileName, path, content) {
+    throw new Error('Not implementd');
+  }
+};
