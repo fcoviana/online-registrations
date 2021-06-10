f3087b7aaac0ef47f3003501d84d9b4e1bbafb2e
Francisco Viana <franciscoviana862@gmail.com>
2021-06-10 07:24:39 -0300

    add interface ExportRegistrationPdfExporter

---
+++ src/app/contracts/export-registration-pdf-exporter.js
@@ -0,0 +1,9 @@
+module.exports = class ExportRegistrationPdfExporter {
+  /*
+  * @param {Registration} registration
+  * @returns {String}
+  */
+  generate(registration) {
+    throw new Error('Not implementd');
+  }
+};
