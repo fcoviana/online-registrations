fb2d729894ee00c568c0b5def82d07a17aa71af0
Francisco Viana <franciscoviana862@gmail.com>
2021-06-10 07:26:48 -0300

    add doc interface LoadRegistationRepository

--- src/domain/repositories/load-registation-repository.js
+++ src/domain/repositories/load-registation-repository.js
@@ -1,4 +1,8 @@
 module.exports = class LoadRegistationRepository {
+  /*
+   * @param {String} cpf
+   * @returns {Registration} registration
+   */
   loadByRegistrationNumber(cpf) {
     throw new Error('Method not implement');
   }
