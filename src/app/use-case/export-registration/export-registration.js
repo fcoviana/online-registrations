ce0f8c473a9336e09e298e49c0bf3390cc846323
Francisco Viana <franciscoviana862@gmail.com>
2021-06-10 07:26:23 -0300

    refactor use-case

--- src/app/use-case/export-registration/export-registration.js
+++ src/app/use-case/export-registration/export-registration.js
@@ -2,14 +2,20 @@ const InputBoundary = require('./input-boundary');
 const OutputBoundary = require('./output-boundary');
 
 module.exports = class ExportRegistration {
-  constructor({ loadRegistationRepository } = {}) {
+  constructor({ loadRegistationRepository, exportRegistrationPdfExporter, storage } = {}) {
     this.loadRegistationRepository = loadRegistationRepository;
+    this.exportRegistrationPdfExporter = exportRegistrationPdfExporter;
+    this.storage = storage;
   }
+
   handle(input) {
     const inputBoundary = new InputBoundary(input);
-    const cpf = inputBoundary;
+    const { cpf, pdfFileName, path } = inputBoundary;
     const registration = this.loadRegistationRepository.loadByRegistrationNumber(cpf);
 
-    return new OutputBoundary(registration);
+    const fileContent = this.exportRegistrationPdfExporter.generate(registration);
+    this.storage.store(pdfFileName, path, fileContent);
+
+    return new OutputBoundary(`${path}/${pdfFileName}`);
   }
 };
