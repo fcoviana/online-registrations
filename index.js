10ef89adf3024e9aaba03d1dd105b4f07c044d9d
Francisco Viana <franciscoviana862@gmail.com>
2021-06-09 21:06:09 -0300

    update index

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const Registration = require('./src/domain/entities/registration');
+const ExportRegistration = require('./src/app/use-case/export-registration/export-registration');
+
+
+class Repository {
+  loadByRegistrationNumber(cpf) {
+    const registration = new Registration({
+      name: 'Francisco',
+      email: 'fco@gmail.com',
+      birthDate: '2000-05-19',
+      registrationNumber: '111.000.111-00',
+      registrationAt: new Date()
+    });
+
+    return registration;
+  }
+}
 
 try {
-  const registration = new Registration({
-    name: 'Francisco',
-    email: 'fco@gmail.com',
-    birthDate: '2000-05-19',
-    registrationNumber: '111.000.111-00',
-    registrationAt: new Date()
+  const exportRegistrationUseCase = new ExportRegistration({
+    loadRegistationRepository: new Repository()
   });
 
-  console.log(registration);
+  console.log(exportRegistrationUseCase.handle('112'));
 } catch (error) {
   console.error(error);
 }
