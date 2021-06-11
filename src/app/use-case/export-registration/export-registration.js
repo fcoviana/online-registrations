f85424495b938fb7f95ab773b0cb5689ba1b7960
Francisco Viana <franciscoviana862@gmail.com>
2021-06-10 23:09:01 -0300

    update use-case

--- src/app/use-case/export-registration/export-registration.js
+++ src/app/use-case/export-registration/export-registration.js
@@ -2,20 +2,20 @@ const InputBoundary = require('./input-boundary');
 const OutputBoundary = require('./output-boundary');
 
 module.exports = class ExportRegistration {
-  constructor({ loadRegistationRepository, exportRegistrationPdfExporter, storage } = {}) {
+  constructor({ loadRegistationRepository, exportRegistrationJsonExporter, storage } = {}) {
     this.loadRegistationRepository = loadRegistationRepository;
-    this.exportRegistrationPdfExporter = exportRegistrationPdfExporter;
+    this.exportRegistrationJsonExporter = exportRegistrationJsonExporter;
     this.storage = storage;
   }
 
-  handle(input) {
+  async handle(input) {
     const inputBoundary = new InputBoundary(input);
     const { cpf, pdfFileName, path } = inputBoundary;
     const registration = this.loadRegistationRepository.loadByRegistrationNumber(cpf);
 
-    const fileContent = this.exportRegistrationPdfExporter.generate(registration);
-    this.storage.store(pdfFileName, path, fileContent);
+    const fileContent = this.exportRegistrationJsonExporter.generate(registration);
+    await this.storage.store(pdfFileName, path, fileContent);
 
-    return new OutputBoundary(`${path}/${pdfFileName}`);
+    return new OutputBoundary(`${path}/${pdfFileName}.json`);
   }
 };
