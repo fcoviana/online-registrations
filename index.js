df9debda9d91eace3b9bcab0dd8f83cbb7ef92b6
Francisco Viana <franciscoviana862@gmail.com>
2021-06-12 10:19:06 -0300

    fix index

--- index.js
+++ index.js
@@ -1,41 +1,26 @@
-const Registration = require("./src/domain/entities/registration");
 const ExportRegistration = require("./src/app/use-case/export-registration/export-registration");
 const JsJsonAdapter = require("./src/infra/adapters/js-json-adapter");
 const LocalStorageAdapter = require("./src/infra/adapters/local-storage-adapter");
-
-class Repository {
-  loadByRegistrationNumber(cpf) {
-    const registration = new Registration({
-      name: "Francisco",
-      email: "fco@gmail.com",
-      birthDate: "2000-05-19",
-      registrationNumber: "111.000.111-00",
-      registrationAt: new Date(),
-    });
-
-    return registration;
-  }
-}
+const KnexRegistrationRepository = require("./src/infra/repositories/mysql/knex/knex-registration-repository");
 
 async function main() {
   try {
     const exportRegistrationUseCase = new ExportRegistration({
-      loadRegistationRepository: new Repository(),
+      registationRepository: new KnexRegistrationRepository(),
       exportRegistrationJsonExporter: new JsJsonAdapter(),
       storage: new LocalStorageAdapter(),
     });
 
     console.log(
       await exportRegistrationUseCase.handle({
-        cpf: "112",
+        cpf: "111.000.111-00ss",
         pdfFileName: `registration_${new Date().getTime()}`,
         path: "/home/fco/workspacer/js/online-registrations/data",
       })
     );
   } catch (error) {
-    console.error(error);
+    console.error(error.stack);
   }
 }
 
 main();
-
